5ab026d299b6cafeec4809ec28f70fec30ad8aa9
Devil1587553128 <150420799+Devil1587553128@users.noreply.github.com>
2023-12-26 12:52:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import yaml
 @pytest.mark.flaky(reruns=6, reruns_delay=2)
 def test_example():
         print(3)
-        assert random.choice([True, False])
+        assert random.choice([True,False])
 
 @pytest.mark.run(order=2)
 def test_foo():
